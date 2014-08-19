4808eccc8a0d38d7d932b065ae508535e3c241a6
Chris <christopherdieckhaus@gmail.com>
2014-08-19 16:31:16 -0400

    Add scroll-down feature to index to focus on images

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,22 +1,35 @@
 $( document ).ready(function() {
   
 //Photo slideshow  
-    var delay = 13000;	
-    var transition_speed = 1000;
+    var delay = 10000;	
+    var transition_speed = 2000;
     var slideshow = $("#slider"),
         photoList = slideshow.children('li'),
         listLength	= photoList.length,
         i	= 0,
 		changePhoto = function (){
-		    photoList.eq(i).fadeOut(transition_speed, function () {
+		    photoList.eq(i).fadeOut(1000, function () {
 				  i += 1;
 				  if (i === listLength) {
 		        i = 0;
 				  }
-				  photoList.eq(i).fadeIn(transition_speed);
+				  photoList.eq(i).hide().fadeIn(transition_speed);
 			   });
 
         };
     photoList.not(':first').hide();
     setInterval(changePhoto, delay);
+  
+    $.fn.scrollView = function () {
+        return this.each(function () {
+        $('html, body').animate({
+          scrollTop: $(this).offset().top
+        }, 1000);
+        });
+    }
+    
+    setTimeout(function(){
+      $('.row#nav').scrollView();
+    },2000); 
+    
 });
