20931c73c296c8a52f7bb36c3c782bd382e9c62b
Chris <christopherdieckhaus@gmail.com>
2014-08-21 13:19:48 -0400

    Automated commit at 2014-08-21 17:19:44 UTC by middleman-deploy 0.2.4

--- javascripts/all.js
+++ javascripts/all.js
@@ -1408,6 +1408,41 @@
     };
   }
 }(jQuery, window, window.document));
+$( document ).ready(function() {
+  
+//Photo slideshow  
+    var delay = 10000;	
+    var transition_speed = 2000;
+    var slideshow = $("#slider"),
+        photoList = slideshow.children('li'),
+        listLength	= photoList.length,
+        i	= 0,
+		changePhoto = function (){
+		    photoList.eq(i).fadeOut(1000, function () {
+				  i += 1;
+				  if (i === listLength) {
+		        i = 0;
+				  }
+				  photoList.eq(i).hide().fadeIn(transition_speed);
+			   });
+
+        };
+    photoList.not(':first').hide();
+    setInterval(changePhoto, delay);
+  
+    $.fn.scrollView = function () {
+        return this.each(function () {
+        $('html, body').animate({
+          scrollTop: $(this).offset().top
+        }, 1000);
+        });
+    }
+    
+    setTimeout(function(){
+      $('.row#nav').scrollView();
+    },3500); 
+    
+});
 /**
  * @preserve FastClick: polyfill to remove click delays on browsers with touch UIs.
  *
