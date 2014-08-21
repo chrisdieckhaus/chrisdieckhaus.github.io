5c5792ff4c2894dfc42a8dc0b58a5ac0e17022e6
Chris <christopherdieckhaus@gmail.com>
2014-08-21 13:17:10 -0400

    Add deploy stuff to config

--- config.rb
+++ config.rb
@@ -80,3 +80,12 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+  deploy.branch = "master"
+  deploy.remote = "git@github.com:chrisdieckhaus/chrisdieckhaus.github.io.git"
+end
+
+
