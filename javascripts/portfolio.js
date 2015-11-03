09f3174211912773d11c8f994ab1b75540851db1
Chris <christopherdieckhaus@gmail.com>
2015-11-03 15:55:38 -0500

    Automated commit at 2015-11-03 20:55:32 UTC by middleman-deploy 1.0.0

---
+++ javascripts/portfolio.js
@@ -0,0 +1,30 @@
+function mouseoverGEC(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/gec.jpg"
+}
+
+function mouseoverDasBoot(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/dasboot1.jpg"
+}
+
+function mouseoverScienceHub(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/scihub_re.png"
+}
+
+function mouseoverCookbook(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/cookbook1.jpg"
+}
+
+function mouseoverPray(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/pray.jpg"
+}
+
+function nextGEC(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/gec3.jpg"
+}
+;
