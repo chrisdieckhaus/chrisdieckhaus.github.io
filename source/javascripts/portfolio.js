62af82af0ba5f162cd69e6ca7d7a473708982c52
Chris <christopherdieckhaus@gmail.com>
2015-11-03 00:36:59 -0500

    Creating portfolio page

---
+++ source/javascripts/portfolio.js
@@ -0,0 +1,9 @@
+function mouseoverGEC(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/gec.jpg"
+}
+
+function mouseoverDasBoot(){
+    var img = document.getElementById("change");
+    img.src = "/images/portfolio/dasboot.jpg"
+}
